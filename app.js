e6886159020360b6dbc8330d9659b3c810165576
moshenahman <jeremy.simha@gmail.com>
2024-05-15 18:01:41 +0300

    Message de commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const AdminBroMongoose = require('@admin-bro/mongoose');
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 AdminBro.registerAdapter(AdminBroMongoose);
 
